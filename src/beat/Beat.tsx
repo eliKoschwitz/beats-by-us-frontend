ac47559537450a63862e216f5aa6fb7128713990
maikpopowicz <maik.popowicz@outlook.com>
2023-01-11 21:49:53 +0100

    feat; delete + navigate

--- src/beat/Beat.tsx
+++ src/beat/Beat.tsx
@@ -2,8 +2,12 @@ import "./beat.css"
 import {BeatType} from "../beatsGallery/BeatsGallery";
 import SoundPads from "../soundPads/SoundPads";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export default function Beat(props:{beat:BeatType}){
+
+    const navigate = useNavigate()
+
 
     async function deleteItem() {
         const response = await axios.delete("/api/beats/" + props.beat.id);
@@ -16,7 +20,7 @@ export default function Beat(props:{beat:BeatType}){
         <>
             <div className={"item-container"}>
                 <h2 className={"beat-title"}>{props.beat.name}</h2>
-                <button className={"button"} onClick={() => deleteItem()}
+                <button className={"button"} onClick={() => { deleteItem(); navigate("/")}}
                 >Delete
                 </button>
             </div>
