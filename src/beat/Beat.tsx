7224279af28c159674df8edde3a60465348e2113
maikpopowicz <maik.popowicz@outlook.com>
2023-01-13 10:51:13 +0100

    fix remove .idea from git

--- src/beat/Beat.tsx
+++ src/beat/Beat.tsx
@@ -8,7 +8,6 @@ export default function Beat(props:{beat:BeatType}){
 
     const navigate = useNavigate()
 
-
     async function deleteItem() {
         const response = await axios.delete("/api/beats/" + props.beat.id);
         console.log(response.status)
@@ -20,7 +19,7 @@ export default function Beat(props:{beat:BeatType}){
         <>
             <div className={"item-container"}>
                 <h2 className={"beat-title"}>{props.beat.name}</h2>
-                <button className={"button"} onClick={() => { deleteItem(); navigate("/")}}
+                <button className={"button"} onClick={() => {deleteItem(); navigate("/")}}
                 >Delete
                 </button>
             </div>
